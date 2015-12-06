0b051b67d5b8456e50000dff9076b878298b8fed
Jou94 <j_jounou94@hotmail.com>
2015-12-06 22:28:01 +0100

    es poden modificar taulells i esborrarlos

--- src/drivers/Funcions.java
+++ src/drivers/Funcions.java
@@ -21,7 +21,7 @@ public class Funcions {
         out.println();
     }
     
-    public static void borrarTaulell(String nomT){ //borrar
+    public static void borrarTaulell(String nomT){ 
         HBD.loadBoards();
         HBD.loadBoardsResolts();
         Boolean done = false;
