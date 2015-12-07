b1bc4c7a92f88b16786fc90fd7ed4d89d19a9237
alexct33 <lex@192.168.1.117>
2015-12-07 02:24:31 +0100

    interficie

--- src/interficie/jugar/Jugar9.java
+++ src/interficie/jugar/Jugar9.java
@@ -252,7 +252,7 @@ public class Jugar9 extends javax.swing.JFrame {
             }
         });
         getContentPane().add(enrere);
-        enrere.setBounds(610, 430, 65, 23);
+        enrere.setBounds(600, 420, 71, 25);
 
         primera33.setEditable(false);
         primera33.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -1076,15 +1076,15 @@ public class Jugar9 extends javax.swing.JFrame {
 
         jButton10.setText("Resoldre");
         getContentPane().add(jButton10);
-        jButton10.setBounds(450, 370, 79, 23);
+        jButton10.setBounds(450, 370, 79, 25);
 
         jButton11.setText("Treure marca");
         getContentPane().add(jButton11);
-        jButton11.setBounds(20, 160, 110, 23);
+        jButton11.setBounds(20, 160, 110, 25);
 
         jButton12.setText("Afegir marca");
         getContentPane().add(jButton12);
-        jButton12.setBounds(20, 120, 100, 23);
+        jButton12.setBounds(20, 120, 100, 25);
 
         numeros1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         numeros1.setText("Números:");
@@ -1136,7 +1136,7 @@ public class Jugar9 extends javax.swing.JFrame {
 
     private void enrereActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_enrereActionPerformed
         // TODO add your handling code here:
-        Menu obj = new Menu();
+        SeleccioTamanyMapa obj = new SeleccioTamanyMapa();
         obj.setVisible(true);
         dispose();
     }//GEN-LAST:event_enrereActionPerformed
