d0a005db95a7d33d8cfe315e611c2e731708486f
Jou94 <j_jounou94@hotmail.com>
2015-12-09 20:25:05 +0100

    carrega matches

--- src/domini/stats/stubGame.java
+++ src/domini/stats/stubGame.java
@@ -1,18 +1,21 @@
 package domini.stats;
+import domini.*;
 
-import domini.Playable;
 
 public class stubGame implements Playable {
 
     int _gameID;
     int _size;
     int _difficulty;
+    public BoardHidato _GameBoard;
+    
     // aqui faltaría un board al que asociar el game, pero no el necesito per Stats.
 
-    public stubGame(int gameID, int size, int difficulty) {
+    public stubGame(int gameID, int size, int difficulty, BoardHidato GameBoard) {
         _gameID = gameID;
         _size = size;
         _difficulty = difficulty;
+        _GameBoard = GameBoard;
     }
 
     public int getID() {
@@ -25,5 +28,9 @@ public class stubGame implements Playable {
 
     public int getDifficulty() {
         return _difficulty;
+    }
+    
+    public BoardHidato getBoard() {
+        return _GameBoard;
     }
 }
