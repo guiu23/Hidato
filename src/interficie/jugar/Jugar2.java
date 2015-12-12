5c01c981684d02129bb64ec1fb5aab2d3cc7fd0b
Jordi Guiu <guiupujols@gmail.com>
2015-12-12 18:50:25 +0100

    passar usuari a tot arreu per saber qui hi ha dins

--- src/interficie/jugar/Jugar2.java
+++ src/interficie/jugar/Jugar2.java
@@ -615,7 +615,7 @@ public class Jugar2 extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Jugar2().setVisible(true);
+                new Jugar2(us).setVisible(true);
             }
         });
     }
